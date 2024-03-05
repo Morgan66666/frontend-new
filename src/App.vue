58688e4151bb91dce27f3f6335407d07708e6e06
cxp03 <cxp030509@gmail.com>
2024-03-05 18:44:00 +0800

    [FrontEnd] Reset specific files to previous states
    - Reverted changes in several files to align with previous versions as part of a rollback or correction process

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,39 @@
 <script setup lang="ts">
 
+import HeaderOfMainPage from "./components/header.vue";
+
 </script>
 
 <template>
-
+  <div class="container">
+    <HeaderOfMainPage></HeaderOfMainPage>
+    <router-view></router-view>
+  </div>
 </template>
 
 <style scoped>
+/*html, body, #app {
+  height: 100%;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}*/
 
+body {
+  padding: 0;
+  margin: 0 0 70px;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+  'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+  sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+.container {
+  height: 100%;
+  width: 100%;
+}
 </style>
