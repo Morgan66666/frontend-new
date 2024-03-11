6d58aa08bdc440b30785c1f6256f4441aabc50f5
DrinkWaterTwice <12110425@mail.sustech.edu.cn>
2024-03-11 17:53:12 +0800

    [FrontEnd] Add Router

---
+++ src/router/index.ts
@@ -0,0 +1,66 @@
+import {createRouter, createWebHistory} from "vue-router";
+import homepage from '../views/homepage.vue'
+import personalPage from '../views/personalPage.vue'
+import commentsList from '../components/personalComponents/commentsList.vue'
+import ProductDetail from "../views/ProductDetail.vue";
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: homepage
+        },
+        {
+            path: '/activity',
+            name: 'activity',
+            component: ProductDetail
+        },
+        {
+            path: '/personal',
+            name: 'personal',
+            component: personalPage,
+            children: [
+                {
+                    path: 'posts',
+                    name: 'posts',
+                    component: commentsList
+                },
+                {
+                    path: 'collected',
+                    component: () => import('../components/personalComponents/collectedList.vue')
+                },
+                {
+                    path: 'comments',
+                    component: () => import('../components/personalComponents/commentsList.vue')
+                },
+                {
+                    path: 'infoEdit',
+                    component: () => import('../components/personalComponents/infoEdit.vue'),
+
+                },
+            ]
+        },
+        {
+            path: '/comments/:id',
+            name: 'comments',
+            component: () => import('../views/viewPage.vue')
+        },
+        {
+            path: '/post-edit',
+            name: 'post-edit',
+            component: () => import('../views/postEditPage.vue')
+        },
+        {
+            name: 'activities',
+            path: '/activities',
+            component: () => import('../views/ActivitiesList.vue'),
+        }
+    ],
+});
+
+
+
+
+export default router;
