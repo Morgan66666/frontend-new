74926296761a497843be6ad1594d6b7d129e05e8
cxp03 <cxp030509@gmail.com>
2024-04-01 02:12:15 +0800

    [FrontEnd] Types

---
+++ src/types/index.ts
@@ -0,0 +1,17 @@
+export interface Product {
+    name: string;
+    category: string;
+    price: string;
+    options: string[];
+    description: string;
+}
+
+export interface Activity {
+    id: string
+    name: string;
+    description: string;
+    location: string;
+    date: string;
+    img: string;
+    price: string;
+}
