30530912c6da6cffac25423395fda0166d28fdf7
Water <qq1754479362@163.com>
2024-04-22 00:10:13 +0800

    ]

--- src/components/loginComponent.vue
+++ src/components/loginComponent.vue
@@ -1,42 +1,42 @@
 <template>
-    <div class="login-card">
-      <div class="login-logo">
-        <img src="../assets/LOGO.svg" alt="SUSTech Logo" >
-      </div>
-      <h1><b>登录</b></h1>
-
-      <div class="input-container">
-        <label :class="{'label-active': active_username}">工号/学号</label>
-        <!--        <input type="text" @focus="activateLabel" @blur="deactivateLabel">-->
-        <input @focus="activateLabel_username" @blur="deactivateLabel_username" v-model="username">
-      </div>
-      <!--      <input type="password" placeholder="密码">-->
-      <div class="input-container">
-        <label :class="{'label-active': active_password}">密码</label>
-        <!--        <input type="text" @focus="activateLabel" @blur="deactivateLabel">-->
-        <input type="password" @focus="activateLabel_password" @blur="deactivateLabel_password" v-model="password">
-      </div>
-      <!--      <p class="hint">Forgot email?</p>-->
-      <!--      <div class="guest-mode">-->
-      <!--        <p>Not your computer? Use Guest mode to sign in privately.</p>-->
-      <!--        <a href="#">Learn more</a>-->
-      <!--      </div>-->
-
-      <!--      <a href="/home">-->
-      <button v-on:click = "login">登录</button>
-      <!--      </a>-->
-
-      <hr class="separate-line">
-
-      <div class="tool">
-        <div class="text-section">
-          <router-link to="#" class="text left">{{ leftText }}</router-link>
-          <hr class="line" />
-          <router-link to="#" class="text right">{{ rightText }}</router-link>
-        </div>
-      </div>
-
+  <div class="login-card">
+    <div class="login-logo">
+      <img src="../assets/LOGO.svg" alt="SUSTech Logo">
     </div>
+    <h1><b>登录</b></h1>
+
+    <div class="input-container">
+      <label :class="{ 'label-active': active_username }">工号/学号</label>
+      <!--        <input type="text" @focus="activateLabel" @blur="deactivateLabel">-->
+      <input @focus="activateLabel_username" @blur="deactivateLabel_username" v-model="username">
+    </div>
+    <!--      <input type="password" placeholder="密码">-->
+    <div class="input-container">
+      <label :class="{ 'label-active': active_password }">密码</label>
+      <!--        <input type="text" @focus="activateLabel" @blur="deactivateLabel">-->
+      <input type="password" @focus="activateLabel_password" @blur="deactivateLabel_password" v-model="password">
+    </div>
+    <!--      <p class="hint">Forgot email?</p>-->
+    <!--      <div class="guest-mode">-->
+    <!--        <p>Not your computer? Use Guest mode to sign in privately.</p>-->
+    <!--        <a href="#">Learn more</a>-->
+    <!--      </div>-->
+
+    <!--      <a href="/home">-->
+    <button v-on:click="login">登录</button>
+    <!--      </a>-->
+
+    <hr class="separate-line">
+
+    <div class="tool">
+      <div class="text-section">
+        <router-link to="#" class="text left">{{ leftText }}</router-link>
+        <hr class="line" />
+        <router-link to="#" class="text right">{{ rightText }}</router-link>
+      </div>
+    </div>
+
+  </div>
 </template>
 
 <script lang="ts">
@@ -73,18 +73,32 @@ export default defineComponent({
       }
     };
 
+    const getJwtFromCookie = () => {
+      const cookies = document.cookie.split(';');
+      for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+        // 如果这个 cookie 的名称是 'jwt'，返回它的值
+        if (cookie.startsWith('jwt=')) {
+          return cookie.substring(4);
+        }
+      }
+      // 如果没有找到名为 'jwt' 的 cookie，返回 null
+      return null;
+    }
+
     const login = async () => {
       let loginForm = {
-        username: username.value,
+        account: username.value,
         password: password.value
       };
 
       console.log("开始登录");
+      console.log(loginForm);
       try {
-        const response = await axios.post('/api/auth/login', loginForm);
+        const response = await axios.post('/api/auth/login', loginForm, {withCredentials: true});
         console.log(response.data);
         if (response.status === 200) {
-          console.log(getCookie('JWT'));
+          console.log(getJwtFromCookie());
           // this.$router.push('/'); // 在 setup 中，需要使用 useRoute 和 useRouter
         } else {
           alert('登录失败');
@@ -168,7 +182,7 @@ label {
   margin: auto;
   padding: 2rem;
   border-radius: 8px;
-  box-shadow: 0 2px 4px rgba(0,0,0,0.2);
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
   background: #fff;
   text-align: center;
 }
@@ -246,19 +260,24 @@ button:hover {
 }
 
 .text-section span {
-  margin: 0 10px; /* 根据需要调整间距 */
+  margin: 0 10px;
+  /* 根据需要调整间距 */
 }
 
 .line {
   flex-grow: 1;
   border: none;
-  border-top: 1px solid #ccc; /* 分割线颜色和样式 */
-  margin: 0 10px; /* 根据需要调整分割线距离文字的间距 */
+  border-top: 1px solid #ccc;
+  /* 分割线颜色和样式 */
+  margin: 0 10px;
+  /* 根据需要调整分割线距离文字的间距 */
 }
 
-.text{
-  text-decoration: none; /* 取消下划线 */
-  color: grey; /* 文字颜色改为灰色 */
+.text {
+  text-decoration: none;
+  /* 取消下划线 */
+  color: grey;
+  /* 文字颜色改为灰色 */
 }
 
 .separate-line {
@@ -268,7 +287,4 @@ button:hover {
 .left {
   margin-right: 120px;
 }
-
-
 </style>
-
